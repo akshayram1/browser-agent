c9209ebad74cb434c1b874a595fe84d607a4a1a8
Akshay Chame <akshaychame2@gmail.com>
2026-03-23 23:35:18 +0530

    fix: update import path for createBrowserAgent to use unpkg

--- examples/simple-site/app.js
+++ examples/simple-site/app.js
@@ -1,4 +1,4 @@
-import { createBrowserAgent } from "../../dist/lib.js";
+import { createBrowserAgent } from "https://unpkg.com/@akshayram1/omnibrowser-agent/dist/lib.js";
 
 const goalEl = document.getElementById("goal");
 const modeEl = document.getElementById("mode");
